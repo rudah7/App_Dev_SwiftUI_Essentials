a01c41f5bda798d37986d8eac6be53fe425868e2
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-06-04 02:29:35 +0900

    Day4_2) Add a theme picker

--- scrumdinger/scrumdinger/DetailEditView.swift
+++ scrumdinger/scrumdinger/DetailEditView.swift
@@ -27,6 +27,8 @@ struct DetailEditView: View {
                     Text("\(scrum.lengthInMinutes) minutes")
                         .accessibilityHidden(true)
                 }
+                
+                ThemePicker(selection: $scrum.theme)
             }
             
             Section(header: Text("Attendees")) {
---
+++ scrumdinger/scrumdinger/ThemePicker.swift
@@ -0,0 +1,28 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by 이예준 on 2023/06/04.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
