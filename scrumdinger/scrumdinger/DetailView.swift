1d61732d3a10ed6c694a2993973ef5f11c957f31
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-06-03 15:46:55 +0900

    Day3_4) Iterate through attendees

--- scrumdinger/scrumdinger/DetailView.swift
+++ scrumdinger/scrumdinger/DetailView.swift
@@ -39,6 +39,12 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
     }
 }
