6257a48f87e135ae0c6fc842c2780bbc9755f7d7
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-06-13 05:24:29 +0900

    Day8_2) Integrate speech recognition

--- scrumdinger/scrumdinger/MeetingView.swift
+++ scrumdinger/scrumdinger/MeetingView.swift
@@ -11,6 +11,9 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    
+    @State private var isRecording = false
     
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
@@ -49,11 +52,21 @@ struct MeetingView: View {
             player.play()
         }
         
+        speechRecognizer.resetTranscript()
+        
+        speechRecognizer.startTranscribing()
+        
+        isRecording = true
+        
         scrumTimer.startScrum()
     }
     
     private func endScrum() {
         scrumTimer.stopScrum()
+        
+        speechRecognizer.stopTranscribing()
+        
+        isRecording = false
         
         let newHistory = History(attendees: scrum.attendees)
         scrum.history.insert(newHistory, at: 0)
